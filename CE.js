373b62319ac653c98cffc7df7a7b131ed6cda272
Your Name <lwaran353@gmail.com>
2021-11-07 20:30:33 +0530

    changes
    changes

--- CE.js
+++ CE.js
@@ -1,5 +1,28 @@
-let inputBtn = document.getElementById("input-btn")
+
+let myLeads=[]
+const inputEl=document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
 
 inputBtn.addEventListener("click",function(){
-    console.log("button is clicked...!")
-})
+    myLeads.push(inputEl.value)
+    inputEl.value=" "
+    renderLead()
+})   
+function renderLead(){
+let vari= ""
+{
+    for(let i=0;i<myLeads.length;i++)
+    {
+      // ulEl.innerHTML+="<li>"+myLeads[i]+"</li>" 
+      //vari+="<li><a target='_blank' href='"+myLeads[i]+"'>"+myLeads[i]+"</a></li>"    
+      vari+=`
+      <li>
+      <a target='_blank' href='${myLeads[i]}'>
+      ${myLeads[i]}
+      </a>
+      </li>`    
+    }
+    ulEl.innerHTML = vari
+}
+}
