9449767af3947932f24485d4dce70c445e24bd37
Your Name <lwaran353@gmail.com>
2021-11-11 19:16:53 +0530

    added del func

--- CE.js
+++ CE.js
@@ -3,11 +3,24 @@ let myLeads=[]
 const inputEl=document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
+const delBtn=document.getElementById("del-btn")
+
+delBtn.addEventListener("dblclick",function(){
+  localStorage.clear()
+  myLeads=[]
+  renderLead()
+})
+
 
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
     inputEl.value=" "
+
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+
     renderLead()
+
+    console.log(localStorage.getItem("myLeads"))
 })   
 function renderLead(){
 let vari= ""
