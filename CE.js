31da11a430bdf5f85b8deb67f90fb5e432491c6a
Your Name <lwaran353@gmail.com>
2021-11-13 19:03:29 +0530

    finished

--- CE.js
+++ CE.js
@@ -1,14 +1,54 @@
-
+let oldLeads=[]
 let myLeads=[]
+const tabs=[
+  {url:"https://www.youtube.com/watch?v=jS4aFq5-91M&t=24786s"}
+]
 const inputEl=document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
 const delBtn=document.getElementById("del-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn=document.getElementById("tab-btn")
+if(leadsFromLocalStorage){
+  myLeads=leadsFromLocalStorage
+  render(myLeads)
+}
+
+tabBtn.addEventListener("click",function(){
+        //myLeads.push(tabs[0].url)
+        //localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        //render(myLeads)
+        chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+        })
+          
+          
+})
+
+function render(leads){
+  let vari= ""
+  {
+      for(let i=0;i<leads.length;i++)
+      {
+        // ulEl.innerHTML+="<li>"+leads[i]+"</li>" 
+        //vari+="<li><a target='_blank' href='"+leads[i]+"'>"+leads[i]+"</a></li>"    
+        vari+=`
+        <li>
+        <a target='_blank' href='${leads[i]}'>
+        ${leads[i]}
+        </a>
+        </li>`    
+      }
+      ulEl.innerHTML = vari
+  }
+  }
 
 delBtn.addEventListener("dblclick",function(){
   localStorage.clear()
   myLeads=[]
-  renderLead()
+  render(myLeads)
 })
 
 
@@ -18,24 +58,7 @@ inputBtn.addEventListener("click",function(){
 
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
 
-    renderLead()
+    render(myLeads)
 
     console.log(localStorage.getItem("myLeads"))
 })   
-function renderLead(){
-let vari= ""
-{
-    for(let i=0;i<myLeads.length;i++)
-    {
-      // ulEl.innerHTML+="<li>"+myLeads[i]+"</li>" 
-      //vari+="<li><a target='_blank' href='"+myLeads[i]+"'>"+myLeads[i]+"</a></li>"    
-      vari+=`
-      <li>
-      <a target='_blank' href='${myLeads[i]}'>
-      ${myLeads[i]}
-      </a>
-      </li>`    
-    }
-    ulEl.innerHTML = vari
-}
-}
